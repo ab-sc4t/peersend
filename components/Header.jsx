cc8d26fe2e59c68734f13ec53365d74f87e8b95a
Ayush Bansal <ayushbansal@192.168.1.7>
2025-03-30 15:29:55 +0530

    inbox component created

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+        <div className="fixed top-0 left-0 w-screen flex justify-between px-8 py-2 bg-white/10 rounded-bl-xl rounded-br-xl">
+            <div className="text-5xl">
+                PeerSend
+            </div>
+            <div className="flex justify-center items-center">
+                <div>
+                    Hello user
+                </div>
+            </div>
+        </div>
+    )
+}

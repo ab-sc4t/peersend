3952d31417fad9ddeb29351a1cc47eaa8f593f6e
Ayush Bansal <ayushbansal@Ayushs-MacBook-Air.local>
2025-04-02 14:38:22 +0530

    Buttons added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,24 @@
-export default function Header() {
+import { authOptions } from "@/lib/auth";
+import Button from "@/ui/Button";
+import { getServerSession } from "next-auth";
+
+export default async function Header() {
+    const session = await getServerSession(authOptions)
+    console.log(session);
     return (
         <div className="fixed top-0 left-0 w-screen flex justify-between px-8 py-2 bg-white/10 rounded-bl-xl rounded-br-xl">
             <div className="text-5xl">
                 PeerSend
             </div>
-            <div className="flex justify-center items-center">
-                <div>
-                    Hello user
-                </div>
+            <div className="flex justify-center items-center gap-4">
+                <Button
+                    text="Register"
+                    href="/signup"
+                />
+                <Button
+                    text="Login"
+                    href="/signin"
+                />
             </div>
         </div>
     )

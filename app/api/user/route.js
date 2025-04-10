2f688d1afe8295c8569ee72aa490b6d78141ef72
Ayush Bansal <ayushbansal@Ayushs-MacBook-Air.local>
2025-04-10 11:04:37 +0530

    priv and pub key generation using mnemonic now

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,7 +1,11 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
 import { db } from "@/lib/db";
+import { generateMnemonic, mnemonicToSeedSync } from "bip39";
+import { derivePath } from "ed25519-hd-key";
 import * as ed from "@noble/ed25519";
+import { sha512 } from "@noble/hashes/sha512";
+
 
 export async function POST(req){
     try{
@@ -15,6 +19,12 @@ export async function POST(req){
         console.log(lastname);
         console.log(username);
         
+        const mnemonic = generateMnemonic();
+        const seed = mnemonicToSeedSync(mnemonic);
+        console.log("Mnemonic: ", mnemonic);
+        const path = `m/44'/501'/0'/0'`;
+        
+        ed.etc.sha512Sync = sha512;
 
         if (!email || !firstname || !lastname || !username) {
             return NextResponse.json({ message: "All fields are required" }, { status: 455 });
@@ -31,16 +41,15 @@ export async function POST(req){
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const privKey = ed.utils.randomPrivateKey();
-        const pubKey = await ed.getPublicKeyAsync(privKey);
-        const privKeyBase64 = Buffer.from(privKey).toString("base64");
-        const pubKeyBase64 = Buffer.from(pubKey).toString("base64");
-        console.log("Public Key: ", pubKeyBase64);
-        console.log("Private Key: ", privKeyBase64);
-
-        const newUser = await db.user.create({data:{email, firstname, lastname, username, publicKey: pubKeyBase64, password: hashedPassword}});
+        const { key: privateKey } = derivePath(path, seed.toString("hex"));
+        const publicKey = await ed.getPublicKey(privateKey);
+        const privateKeyBaseHex = Buffer.from(privateKey).toString("hex")
+        const publicKeyBaseHex = Buffer.from(publicKey).toString("hex")
+        console.log("Private Key (hex):", Buffer.from(privateKey).toString("hex"));
+        console.log("Public Key  (hex):", Buffer.from(publicKey).toString("hex"));
+        const newUser = await db.user.create({data:{email, firstname, lastname, username, publicKey: publicKeyBaseHex, password: hashedPassword}});
         const { password: _, ...safeUserData } = newUser;
-        return NextResponse.json({ ...safeUserData, privateKey: privKeyBase64 }, { status: 201 }); 
+        return NextResponse.json({ ...safeUserData, privateKey: privateKeyBaseHex, mnemonic: mnemonic }, { status: 201 }); 
         
     } catch(error){
         console.error("Signup error:", error);

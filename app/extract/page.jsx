65f9652ad4b409438ba8df8b1d9c704522fc5387
ab-sc4t <ab.ayush2612@gmail.com>
2025-05-08 12:01:53 +0530

    on logout redirect to /signin page

--- app/extract/page.jsx
+++ app/extract/page.jsx
@@ -1,9 +1,13 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation"; 
 import ExtractPrivKeyClient from "@/components/ExtractPrivKeyClient";
 
 export default async function ExtractPrivKeyPage() {
     const session = await getServerSession(authOptions);
-
+    if (!session) {
+        redirect("/signin");
+        return; 
+    }
     return <ExtractPrivKeyClient session={session} />;
 }
